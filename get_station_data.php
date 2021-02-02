04576dd72ee043229e36b3692ae30b69a9a4748f
Wouter Gritter <woutergritter@gmail.com>
2021-02-02 10:15:19 +0100

    Add country selection

--- get_station_data.php
+++ get_station_data.php
@@ -15,13 +15,13 @@ if($type == 0){
   $result = $database_connection->query("SELECT * FROM data WHERE station_id =".$id." ORDER BY ABS(date - ".$date.") ASC LIMIT 1");
 }
 elseif($type == 1){
-  $result = $database_connection->query("SELECT * FROM data WHERE date BETWEEN (SELECT date FROM data ORDER BY ABS(date - ".$date.") ASC LIMIT 1) AND ((SELECT date FROM data ORDER BY ABS(date - ".$date.") ASC LIMIT 1) + 50) ORDER BY snow_height DESC LIMIT 1");
+  $result = $database_connection->query("SELECT * FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') AND date BETWEEN (SELECT date FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') ORDER BY ABS(date - ".$date.") ASC LIMIT 1) AND ((SELECT date FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') ORDER BY ABS(date - ".$date.") ASC LIMIT 1) + 50) ORDER BY snow_height DESC LIMIT 1");
 }
 elseif($type == 2){
-  $result = $database_connection->query("SELECT * FROM data WHERE date BETWEEN (SELECT date FROM data ORDER BY ABS(date - ".$date.") ASC LIMIT 1) AND ((SELECT date FROM data ORDER BY ABS(date - ".$date.") ASC LIMIT 1) + 50) ORDER BY temperature DESC LIMIT 1");
+  $result = $database_connection->query("SELECT * FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') AND date BETWEEN (SELECT date FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') ORDER BY ABS(date - ".$date.") ASC LIMIT 1) AND ((SELECT date FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') ORDER BY ABS(date - ".$date.") ASC LIMIT 1) + 50) ORDER BY temperature DESC LIMIT 1");
 }
 elseif($type == 3){
-  $result = $database_connection->query("SELECT * FROM data WHERE date BETWEEN (SELECT date FROM data ORDER BY ABS(date - ".$date.") ASC LIMIT 1) AND ((SELECT date FROM data ORDER BY ABS(date - ".$date.") ASC LIMIT 1) + 50) ORDER BY temperature LIMIT 1");
+  $result = $database_connection->query("SELECT * FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') AND date BETWEEN (SELECT date FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') ORDER BY ABS(date - ".$date.") ASC LIMIT 1) AND ((SELECT date FROM data WHERE (country='NORWAY' OR country='SWEDEN' OR country='DENMARK' OR country='ICELAND' OR country='FINLAND' OR country='FAROE ISLANDS') ORDER BY ABS(date - ".$date.") ASC LIMIT 1) + 50) ORDER BY temperature ASC  LIMIT 1");
 }
 
 $answer = array();
